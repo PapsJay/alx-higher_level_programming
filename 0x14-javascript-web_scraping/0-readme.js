9a51c846f1a362e3bd17495ba1c1363fa1eb62ef
papsjay <donallotei@gmail.com>
2023-02-09 14:35:14 -0800

    0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
-
-const filePath = process.argv[2];
 const fs = require('fs');
-fs.readFile(filePath, 'utf8', function (err, data) {
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(data.toString());
   }
 });
